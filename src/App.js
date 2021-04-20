358bf0dcc8c9f627fff8e0403522efbac84db5ff
Mohan0078 <mohanpatel.mp73@gmail.com>
2021-04-20 18:33:24 +0530

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import Card from './Card';
 import TodoList from './TodoList';
-import Bootstrape_Demo from './Bootstrape_Demo';
 
 const App = () => {
   return (
      
     <>
     <TodoList/>
-     {/* <Card/>  */}
-     <br/><br/>
-     {/* <Bootstrape_Demo/> */}
+    <br /><br />
     </>
   );
 };
